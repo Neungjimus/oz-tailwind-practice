69344b931a761d4b54b11d5a7efda4e6aea0f700
Neungjimus <lingchihhao@gmail.com>
2025-07-11 20:25:05 +0900

    tailwind-practice 과제 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className="flex justify-between items-center px-5 py-2 bg-black">
+      <h1 className="text-white text-xl font-bold">OZ코딩스쿨</h1>
+      <ul className="flex gap-5 text-white text-sm font-normal">
+        <li className="cursor-pointer hover:underline">로그인</li>
+        <li className="cursor-pointer hover:underline">회원가입</li>
+        <li className="cursor-pointer hover:underline">내클래스</li>
       </ul>
     </header>
   );
